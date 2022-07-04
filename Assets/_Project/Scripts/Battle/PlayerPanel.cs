f6d8d0286545396b11a48c8a02e27efb38a85866
l.hilgevoord <lucashilgevoord@hotmail.com>
2022-07-04 14:18:22 +0200

    Added sprites, renamed files

--- Assets/_Project/Scripts/Battle/PlayerPanel.cs
+++ Assets/_Project/Scripts/Battle/PlayerPanel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BattlePlayerPanel : MonoBehaviour
+public class PlayerPanel : MonoBehaviour
 {
     private float _startY = 0;
     private bool _isEnabled = true;
